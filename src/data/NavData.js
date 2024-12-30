1c24acc1259d209ed3189ce42d3cb7cb17c97895
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-30 18:59:24 +0530

    Add a basic navbar and a basic hero section

---
+++ src/data/NavData.js
@@ -0,0 +1,20 @@
+const navData = [
+    {
+        title: 'Home',
+        href: '/'
+    },
+    {
+        title: 'Projects',
+        href: '#projects'
+    },
+    {
+        title: 'About',
+        href: '#about'
+    },
+    {
+        title: 'Goals',
+        href: '#goals'
+    },
+]
+
+export default navData;
