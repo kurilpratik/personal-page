36e2ba66a65285020d5a1e0544f642ec7d0f9847
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-31 23:03:48 +0530

    Create the goals section

---
+++ src/data/ListItems.js
@@ -0,0 +1,19 @@
+const ListItems = [
+    {
+        title: ' My Goals',
+        color: '#D9EDFE',
+        darkerColor: '#65B8FF',
+        arr: ['Solve 200 LC', 'Launch CampusOne', 'Get an internship', 'Score a 50', '...and more'],
+        tilt: '-2deg'
+    },
+    {
+        title: 'Things I love',
+        color: '#E5D6FD',
+        darkerColor: '#AB77FF',
+        arr: ['Books', 'Design', 'Cricket', 'Websites', 'Snoopy'],
+        tilt: '12deg'
+    },
+    
+]
+
+export default ListItems;
