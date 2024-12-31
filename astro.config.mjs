b65e20b45e09b8bccaf62f3ac9e2c10ad87178fa
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-31 23:13:23 +0530

    Add netlify hosting capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
   // vite: {
   //   css: {
   //     postcss: './postcss.config.cjs',
   //   }
   // }
+  integrations: [tailwind(), react()],
+
+  adapter: netlify()
 });
