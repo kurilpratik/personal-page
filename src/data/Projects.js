1ea12a64810a75b2fee69888ffa6eb4ee9ddd1f0
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-31 23:27:42 +0530

    Shift assets to public and change src accordingly

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -3,19 +3,19 @@ const ProjectData = [
     {
         title: 'Galgotias International Model United Nations Website',
         description: 'Model United Nations, as a concept, for me has always been associated with challenging thoughts and proposing innovative solutions to complex global problems. GIMUN was built to represent and act as a single source of information for the first of its kind campus event.',
-        imgsrc: '../src/assets/gimun.png',
+        imgsrc: '/gimun.png',
         link: 'https://gimun.in'
     },
     {
         title: 'Her Haq Organization Website',
         description: 'Her Haq is a youth led not-for-profit organization that works towards gender equality and women empowerment. It is the successor of the student collective, Girl Up Rise (a UN Foundation initiative).  We wish to create an ideal world where no woman has to fight to make her voice heard.',
-        imgsrc: '../src/assets/herhaq.png',
+        imgsrc: '/herhaq.png',
         link: 'https://herhaq.com'
     },
     {
         title: 'Campus One Website',
         description: 'Explore and engage with students of the college and spark interesting and productive conversations. This is an effort (a big one) towards building a vibrant community of forward thinkers by recognizing talent, boosting collaboration and generating true college culture.',
-        imgsrc: '../src/assets/campusone.png',
+        imgsrc: '/campusone.png',
         link: 'https://campusone.live'
     }
 ]
